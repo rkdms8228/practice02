67ff9ed70fb259924c22d947ba4385a4a6697233
가은 <user@192.168.45.72>
2022-04-16 22:52:22 +0900

    ex01

--- src/com/javaex/practice/Ex01.java
+++ src/com/javaex/practice/Ex01.java
@@ -1,5 +1,21 @@
 package com.javaex.practice;
 
+import java.util.Scanner;
+
 public class Ex01 {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int age = 15;
+		
+		if (age>0 && age<18) {
+			System.out.println("청소년입니다.");
+		}
+		
+		sc.close();
+		
+	}
 
 }
